177fba0492cdb43d2c938bf11b1f3c0ee844388d
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2020-11-16 15:23:58 +0100

    the solution for the clock exercise

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,3 +1,16 @@
-test('renders the clock component on the base route', () => {});
+import {render, screen} from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom';
+import App from '../App';
+
+const renderWithRouter = (ui, {route = '/'} = {}) => {
+  return render(ui, {wrapper: BrowserRouter});
+};
+
+test('renders the clock component on the base route', () => {
+  renderWithRouter(<App />);
+
+  const clockComponent = screen.getByTestId('clock');
+  expect(clockComponent).toBeDefined();
+});
 
 test('renders the stopwatch component on the /stopwatch route', () => {});
--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 
 const Clock = () => {
+  const [currentTime, setCurrentTime] = React.useState(new Date());
+  const clockRef = React.useRef();
+
+  React.useEffect(() => {
+    clockRef.current = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(clockRef.current);
+    };
+  }, []);
+
+  const seconds = currentTime.getSeconds();
+  const minutes = currentTime.getMinutes();
+  const hours = currentTime.getHours();
+
+  const secondsStyle = {transform: `rotate(${seconds * 6}deg)`};
+  const minutesStyle = {transform: `rotate(${minutes * 6}deg)`};
+  const hoursStyle = {transform: `rotate(${hours * 30}deg)`};
+
   return (
     <div className="page-container">
       <div data-testid="clock" className="clock mt-5">
@@ -10,9 +31,9 @@ const Clock = () => {
           <div className="marking marking-three"></div>
           <div className="marking marking-four"></div>
           <div className="inner-clock-face">
-            <div className="hand hour-hand"></div>
-            <div className="hand min-hand"></div>
-            <div className="hand second-hand"></div>
+            <div className="hand hour-hand" style={hoursStyle}></div>
+            <div className="hand min-hand" style={minutesStyle}></div>
+            <div className="hand second-hand" style={secondsStyle}></div>
           </div>
         </div>
       </div>
