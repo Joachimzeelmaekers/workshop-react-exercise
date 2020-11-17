15c171a1cc0a8306e09760368a6f1150bf0b9eb9
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2020-11-17 12:25:41 +0100

    final changes

--- src/pages/showsOverview/ShowsOverview.jsx
+++ src/pages/showsOverview/ShowsOverview.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useMemo} from 'react';
+import React, {useEffect, useState} from 'react';
 import Card from '../../components/Card';
 import Search from '../../components/Search';
 
